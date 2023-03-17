ea5eee9830f3fa2881e4ddd213a67bddc1563471
Dir-A <g2576612205@gmail.com>
2023-03-17 15:42:18 +0800

    nop

---
+++ ProxyDll/msimg32/msimg32.cpp
@@ -0,0 +1,28 @@
+#include "msimg32.h"
+
+
+DWORD g_dwExeImageBase = (DWORD)GetModuleHandleW(NULL);
+
+
+VOID StartHook()
+{
+	// To Do
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		ProxyDll();
+		StartHook();
+		break;
+	case DLL_THREAD_ATTACH:
+		break;
+	case DLL_THREAD_DETACH:
+		break;
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
