b04e23ba82d56d910ac2089f41061ee61b069149
Dir-A <g2576612205@gmail.com>
2023-08-21 17:49:55 +0800

    add record

---
+++ lib/Rut/RxBench.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "RxBench/Record.h"
--- src/RiaMainTest/main.cpp
+++ src/RiaMainTest/main.cpp
@@ -1,46 +1,12 @@
 #include "../../lib/Rut/RxStream.h"
-#include <iostream>
-#include <chrono>
-
-class RxRecord
-{
-private:
-	std::chrono::steady_clock::time_point m_tpBeg;
-	std::chrono::steady_clock::time_point m_tpEnd;
-	std::vector<std::chrono::duration<double, std::milli>> m_vcRecord;
-
-public:
-	inline void Beg()
-	{
-		m_tpBeg = std::chrono::steady_clock::now();
-	}
-
-	inline void End()
-	{
-		m_tpEnd = std::chrono::steady_clock::now();
-		m_vcRecord.emplace_back((m_tpEnd - m_tpBeg));
-	}
-
-	void Log()
-	{
-		std::chrono::duration<double, std::milli> cout{};
-
-		for (auto& dur : m_vcRecord)
-		{
-			cout += dur;
-			std::cout << dur << "\n";
-		}
-
-		std::cout << "Avg:" << cout / m_vcRecord.size() << "\n";
-
-	}
-};
+#include "../../lib/Rut/RxBench.h"
 
 using namespace Rut;
 
+
 int main()
 {
-	RxRecord record;
+	RxBench::Record record;
 
 	std::vector<std::wstring> text_list;
 	for (size_t i = 0; i < 50; i++)
@@ -54,7 +20,5 @@ int main()
 	}
 
 	record.Log();
-
-
 
 }
